2a7a973a44860c2ea7d8fc148abef258ace51868
xuananh2212 <xuantuananh2212@gmail.com>
2023-10-24 17:33:20 +0700

    add vercel.json

---
+++ .history/day40/module_bundler/src/Layouts/Default_20231024171159.js
@@ -0,0 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Assets/Style.scss";
+export function DefaultLayout() {
+     return `
+     <header class="mb-3">
+     <div class="container">
+     <h1><a href="/" data-route>Header</a></h1>
+     </div>
+     </header>
+     <hr>
+     <main>
+     <div class="container">
+     <div class="row">
+          <div class="col-3">
+               <h2>Menu</h2>
+               <ul>
+                    <li><a href="/" data-route>Trang Chủ</a></li>
+                    <li><a href="/gioi-thieu" data-route>Giới thiệu</a></li>
+                    <li><a href="/san-pham" data-route>Sản Phẩm</a></li>
+               </ul>
+          </div>
+          <div class="col-9">
+               {body}
+          </div>
+     </div>
+     </div>
+     </main>
+     <footer class="mt-3">
+          <div class="container">
+               <h1>FOOTER</h1>
+          </div>
+     </footer>
+     `
+}
